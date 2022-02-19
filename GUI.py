4830c0efcbe829ec06426499b2a65addee7d2e2e
Michael Perry <mperry03@syr.edu>
2022-02-19 15:17:14 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,14 +3,29 @@ from graphics import *
 def main():
     win = GraphWin("Tic-Tac-Joe", 800, 800)
     win.setCoords(0, 0, 11, 11)
+
     BB_line1 = Line(Point(4, 1), Point(4, 10))
     BB_line2 = Line(Point(7, 1), Point(7, 10))
     BB_line3 = Line(Point(1, 4), Point(10, 4))
     BB_line4 = Line(Point(1, 7), Point(10, 7))
-    BB_line1.draw(win)
-    BB_line2.draw(win)
-    BB_line3.draw(win)
-    BB_line4.draw(win)
+
+    BB = [BB_line1, BB_line2, BB_line3, BB_line4]
+    for item in BB:
+        item.setWidth(3)
+        item.draw(win)
+
+    SB = []
+    for k in range(3):
+        for j in range(3):
+            for i in range(2):
+                line = Line(Point(3 * j + 2 + i, 3 * k + 1.25), Point(3 * j + 2 + i, 3 * k + 3.75))
+                SB.append(line)
+                line = Line(Point(3 * j + 1.25, 3 * k + 2 + i), Point(3 * j + 3.75, 3 * k + 2 + i))
+                SB.append(line)
+
+    for item in SB:
+        item.setWidth(3)
+        item.draw(win)
 
     win.getMouse()
     win.close()
