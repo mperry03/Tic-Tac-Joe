6f2694aa9f6cc13f6343fb48fdfc23669392ff00
Michael Perry <mperry03@syr.edu>
2022-02-20 08:46:36 -0500

    no joe glitch

--- main/tictacjoe.py
+++ main/tictacjoe.py
@@ -9,12 +9,19 @@ def instantiate_AI():
 
 class AI():
 
+    def __init__(self):
+        self.state = True
+
+    def setState(self, state):
+        self.state = state
+
     def chooseMove(self,board):
-        possibleMoves = []
-        for i in range(9):
-            for j in range(9):
-                if board.is_valid_move([i,j]):
-                    possibleMoves.append([i,j])
-        return random.choice(possibleMoves)
+        if self.state == True:
+            possibleMoves = []
+            for i in range(9):
+                for j in range(9):
+                    if board.is_valid_move([i,j]):
+                        possibleMoves.append([i,j])
+            return random.choice(possibleMoves)
 
 
